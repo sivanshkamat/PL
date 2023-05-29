0442a8f4ec58373ec0465c86662694c3fd521866
sivanshkamat <ksivansh@gmail.com>
2023-05-29 21:55:51 +0545

    code refactored

--- src/app/points-table/points-table.component.ts
+++ src/app/points-table/points-table.component.ts
@@ -4,6 +4,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {NgFor, NgIf} from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
+import { TeamDetails, TeamList } from '../team-detail/teamlist';
+
 
 /**
  * @title Table with expandable rows
@@ -23,39 +25,8 @@ import {MatTableModule} from '@angular/material/table';
   imports: [MatTableModule, NgFor, MatButtonModule, NgIf, MatIconModule],
 })
 export class PointsTableComponent {
-  dataSource = ELEMENT_DATA;
+  dataSource = TeamList;
   columnsToDisplay = ['name', 'matches', 'won', 'lost', 'draw', 'totalgoals', 'points'];
   expandedTeam: TeamDetails | any;
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
 }
-
-export interface TeamDetails {
-  name: string;
-  matches: number;
-  won: number;
-  lost: number;
-  draw: number;
-  totalgoals: number;
-  points: number;
-}
-
-const ELEMENT_DATA: TeamDetails[] = [
-  {
-    name: 'Man City',
-    matches: 5,
-    won: 2,
-    lost: 1,
-    draw: 2,
-    totalgoals:7,
-    points: 21
-  },
-  {
-    name: 'Arsenal',
-    matches: 5,
-    won: 2,
-    lost: 1,
-    draw: 2,
-    totalgoals:7,
-    points: 21
-  },
-];
