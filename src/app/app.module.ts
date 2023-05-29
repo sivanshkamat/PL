010a6270eeaa55a4d440d47897fa8dcb067d04c3
sivanshkamat <ksivansh@gmail.com>
2023-05-29 16:48:11 +0545

    firebase was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import {MatDialogModule} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component'; //dialog
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { TeamDetailComponent } from './dialog/team-detail/team-detail.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +31,10 @@ import { TeamDetailComponent } from './dialog/team-detail/team-detail.component'
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
-    DialogComponent
+    DialogComponent,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
