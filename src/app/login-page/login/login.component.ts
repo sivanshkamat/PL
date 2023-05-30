ca936e35bc0527852c51ec9337e44815fb3d2f6c
sivanshkamat <ksivansh@gmail.com>
2023-05-31 01:08:13 +0545

    Firebase: Login set up

--- src/app/login-page/login/login.component.ts
+++ src/app/login-page/login/login.component.ts
@@ -1,17 +1,55 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-});
-constructor() {};
+  constructor(
+    private authService: AuthService,
+    private toast: HotToastService,
+    private router: Router,
+    private fb: NonNullableFormBuilder
+  ) {}
 
+  ngOnInit(): void {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  submit() {
+    const { email, password } = this.loginForm.value;
+
+    if (!this.loginForm.valid || !email || !password) {
+      return;
+    }
+
+    this.authService
+      .login(email, password)
+      .pipe(
+        this.toast.observe({
+          success: 'Logged in successfully',
+          loading: 'Logging in...',
+          error: ({ message }) => `There was an error: ${message} `,
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+  }
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {
+  collection,
+  doc,
+  docData,
+  Firestore,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { filter, from, map, Observable, of, switchMap } from 'rxjs';
+import { AuthService } from './auth.service';
+
+interface ProfileUser {
+  uid: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  displayName?: string;
+  phone?: string;
+  address?: string;
+  photoURL?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private firestore: Firestore, private authService: AuthService) {}
+
+  get currentUserProfile$(): Observable<ProfileUser | null> {
+    return this.authService.currentUser$.pipe(
+      switchMap((user) => {
+        if (!user?.uid) {
+          return of(null);
+        }
+
+        const ref = doc(this.firestore, 'users', user?.uid);
+        return docData(ref) as Observable<ProfileUser>;
+      })
+    );
+  }
+
+  addUser(user: ProfileUser): Observable<void> {
+    const ref = doc(this.firestore, 'users', user.uid);
+    return from(setDoc(ref, user));
+  }
+
+  updateUser(user: ProfileUser): Observable<void> {
+    const ref = doc(this.firestore, 'users', user.uid);
+    return from(updateDoc(ref, { ...user }));
+  }
+}
